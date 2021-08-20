72f9eb7ccc5421459c8b2999132e80d778e75143
therealyingtong <yingtong@z.cash>
2021-08-20 18:44:39 +0800

    sinsemilla::merkle.rs: Implement i2lebsp locally.
    Also include gen_const_array helper in gadget::utilities.

--- src/circuit/gadget/utilities.rs
+++ src/circuit/gadget/utilities.rs
@@ -165,6 +165,18 @@ pub fn decompose_word<F: PrimeFieldBits>(
         .collect()
 }
 
+/// Takes in an FnMut closure and returns a constant-length array with elements of
+/// type `Output`.
+pub fn gen_const_array<Output: Copy + Default, const LEN: usize>(
+    mut closure: impl FnMut(usize) -> Output,
+) -> [Output; LEN] {
+    let mut ret: [Output; LEN] = [Default::default(); LEN];
+    for (bit, val) in ret.iter_mut().zip((0..LEN).map(|idx| closure(idx))) {
+        *bit = val;
+    }
+    ret
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
