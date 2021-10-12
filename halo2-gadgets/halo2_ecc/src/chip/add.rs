therealyingtong/halo2-gadgets

Refactor ECC tests to be generic over fixed bases.
This removes the need to import OrchardFixedBases to the halo2_ecc
crate.

halo2-gadgets/halo2_ecc/src/chip/add.rs
@@ -385,9 +385,9 @@ impl Config {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
-    use group::{prime::PrimeCurveAffine, Curve};
+    use group::{prime::PrimeCurveAffine, Curve, Group};
     use halo2::{circuit::Layouter, plonk::Error};
     use pasta_curves::{arithmetic::CurveExt, pallas};
 
@@ -397,31 +397,44 @@ pub mod tests {
     pub fn test_add<EccChip: EccInstructions<pallas::Affine> + Clone + Eq + std::fmt::Debug>(
         chip: EccChip,
         mut layouter: impl Layouter<pallas::Base>,
-        zero: &Point<pallas::Affine, EccChip>,
-        p_val: pallas::Affine,
-        p: &Point<pallas::Affine, EccChip>,
-        q_val: pallas::Affine,
-        q: &Point<pallas::Affine, EccChip>,
-        p_neg: &Point<pallas::Affine, EccChip>,
     ) -> Result<(), Error> {
+        // Generate a random point P
+        let p_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // P
+        let p = Point::new(chip.clone(), layouter.namespace(|| "P"), Some(p_val))?;
+        let p_neg = -p_val;
+        let p_neg = Point::new(chip.clone(), layouter.namespace(|| "-P"), Some(p_neg))?;
+
+        // Generate a random point Q
+        let q_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // Q
+        let q = Point::new(chip.clone(), layouter.namespace(|| "Q"), Some(q_val))?;
+
         // Make sure P and Q are not the same point.
         assert_ne!(p_val, q_val);
 
+        // Generate a (0,0) point to be used in other tests.
+        let zero = {
+            Point::new(
+                chip.clone(),
+                layouter.namespace(|| "identity"),
+                Some(pallas::Affine::identity()),
+            )?
+        };
+
         // Check complete addition P + (-P)
         {
-            let result = p.add(layouter.namespace(|| "P + (-P)"), p_neg)?;
-            result.constrain_equal(layouter.namespace(|| "P + (-P) = 𝒪"), zero)?;
+            let result = p.add(layouter.namespace(|| "P + (-P)"), &p_neg)?;
+            result.constrain_equal(layouter.namespace(|| "P + (-P) = 𝒪"), &zero)?;
         }
 
         // Check complete addition 𝒪 + 𝒪
         {
-            let result = zero.add(layouter.namespace(|| "𝒪 + 𝒪"), zero)?;
-            result.constrain_equal(layouter.namespace(|| "𝒪 + 𝒪 = 𝒪"), zero)?;
+            let result = zero.add(layouter.namespace(|| "𝒪 + 𝒪"), &zero)?;
+            result.constrain_equal(layouter.namespace(|| "𝒪 + 𝒪 = 𝒪"), &zero)?;
         }
 
         // Check P + Q
         {
-            let result = p.add(layouter.namespace(|| "P + Q"), q)?;
+            let result = p.add(layouter.namespace(|| "P + Q"), &q)?;
             let witnessed_result = Point::new(
                 chip.clone(),
                 layouter.namespace(|| "witnessed P + Q"),
@@ -432,7 +445,7 @@ pub mod tests {
 
         // P + P
         {
-            let result = p.add(layouter.namespace(|| "P + P"), p)?;
+            let result = p.add(layouter.namespace(|| "P + P"), &p)?;
             let witnessed_result = Point::new(
                 chip.clone(),
                 layouter.namespace(|| "witnessed P + P"),
@@ -443,14 +456,14 @@ pub mod tests {
 
         // P + 𝒪
         {
-            let result = p.add(layouter.namespace(|| "P + 𝒪"), zero)?;
-            result.constrain_equal(layouter.namespace(|| "P + 𝒪 = P"), p)?;
+            let result = p.add(layouter.namespace(|| "P + 𝒪"), &zero)?;
+            result.constrain_equal(layouter.namespace(|| "P + 𝒪 = P"), &p)?;
         }
 
         // 𝒪 + P
         {
-            let result = zero.add(layouter.namespace(|| "𝒪 + P"), p)?;
-            result.constrain_equal(layouter.namespace(|| "𝒪 + P = P"), p)?;
+            let result = zero.add(layouter.namespace(|| "𝒪 + P"), &p)?;
+            result.constrain_equal(layouter.namespace(|| "𝒪 + P = P"), &p)?;
         }
 
         // (x, y) + (ζx, y) should behave like normal P + Q.

halo2-gadgets/halo2_ecc/src/chip/add_incomplete.rs
@@ -142,9 +142,9 @@ impl Config {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
-    use group::Curve;
+    use group::{prime::PrimeCurveAffine, Curve, Group};
     use halo2::{circuit::Layouter, plonk::Error};
     use pasta_curves::pallas;
 
@@ -156,16 +156,32 @@ pub mod tests {
     >(
         chip: EccChip,
         mut layouter: impl Layouter<pallas::Base>,
-        zero: &Point<pallas::Affine, EccChip>,
-        p_val: pallas::Affine,
-        p: &Point<pallas::Affine, EccChip>,
-        q_val: pallas::Affine,
-        q: &Point<pallas::Affine, EccChip>,
-        p_neg: &Point<pallas::Affine, EccChip>,
     ) -> Result<(), Error> {
+        // Generate a random point P
+        let p_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // P
+        let p = Point::new(chip.clone(), layouter.namespace(|| "P"), Some(p_val))?;
+        let p_neg = -p_val;
+        let p_neg = Point::new(chip.clone(), layouter.namespace(|| "-P"), Some(p_neg))?;
+
+        // Generate a random point Q
+        let q_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // Q
+        let q = Point::new(chip.clone(), layouter.namespace(|| "Q"), Some(q_val))?;
+
+        // Make sure P and Q are not the same point.
+        assert_ne!(p_val, q_val);
+
+        // Generate a (0,0) point to be used in other tests.
+        let zero = {
+            Point::new(
+                chip.clone(),
+                layouter.namespace(|| "identity"),
+                Some(pallas::Affine::identity()),
+            )?
+        };
+
         // P + Q
         {
-            let result = p.add_incomplete(layouter.namespace(|| "P + Q"), q)?;
+            let result = p.add_incomplete(layouter.namespace(|| "P + Q"), &q)?;
             let witnessed_result = Point::new(
                 chip,
                 layouter.namespace(|| "witnessed P + Q"),
@@ -175,23 +191,23 @@ pub mod tests {
         }
 
         // P + P should return an error
-        p.add_incomplete(layouter.namespace(|| "P + P"), p)
+        p.add_incomplete(layouter.namespace(|| "P + P"), &p)
             .expect_err("P + P should return an error");
 
         // P + (-P) should return an error
-        p.add_incomplete(layouter.namespace(|| "P + (-P)"), p_neg)
+        p.add_incomplete(layouter.namespace(|| "P + (-P)"), &p_neg)
             .expect_err("P + (-P) should return an error");
 
         // P + 𝒪 should return an error
-        p.add_incomplete(layouter.namespace(|| "P + 𝒪"), zero)
+        p.add_incomplete(layouter.namespace(|| "P + 𝒪"), &zero)
             .expect_err("P + 0 should return an error");
 
         // 𝒪 + P should return an error
-        zero.add_incomplete(layouter.namespace(|| "𝒪 + P"), p)
+        zero.add_incomplete(layouter.namespace(|| "𝒪 + P"), &p)
             .expect_err("0 + P should return an error");
 
         // 𝒪 + 𝒪 should return an error
-        zero.add_incomplete(layouter.namespace(|| "𝒪 + 𝒪"), zero)
+        zero.add_incomplete(layouter.namespace(|| "𝒪 + 𝒪"), &zero)
             .expect_err("𝒪 + 𝒪 should return an error");
 
         Ok(())

halo2-gadgets/halo2_ecc/src/chip/mul_fixed/base_field_elem.rs
@@ -1,6 +1,6 @@
 use super::super::{
     EccBaseFieldElemFixed, EccConfig, EccPoint, FixedPoints, FIXED_BASE_WINDOW_SIZE, LOOKUP_K,
-    L_ORCHARD_BASE, NUM_WINDOWS, T_P,
+    L_PALLAS_BASE, NUM_WINDOWS, T_P,
 };
 use super::H_BASE;
 use utilities::{
@@ -170,7 +170,7 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
                         offset,
                         scalar,
                         true,
-                        L_ORCHARD_BASE,
+                        L_PALLAS_BASE,
                         NUM_WINDOWS,
                     )?;
                     EccBaseFieldElemFixed {
@@ -372,9 +372,9 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
-    use group::Curve;
+    use group::{Curve, Group};
     use halo2::{
         circuit::{Chip, Layouter},
         plonk::Error,
@@ -382,41 +382,47 @@ pub mod tests {
     use pasta_curves::{arithmetic::FieldExt, pallas};
 
     use crate::{
-        chip::EccChip,
+        chip::{EccChip, NUM_WINDOWS},
         gadget::{FixedPoint, FixedPoints, Point, H},
     };
-    use orchard::constants::OrchardFixedBases;
     use utilities::UtilitiesInstructions;
 
-    pub fn test_mul_fixed_base_field(
-        chip: EccChip<OrchardFixedBases>,
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref BASE: pallas::Affine = pallas::Point::generator().to_affine();
+        static ref ZS_AND_US: Vec<(u64, [[u8; 32]; H])> =
+            crate::chip::find_zs_and_us(*BASE, NUM_WINDOWS).unwrap();
+    }
+
+    pub fn test_mul_fixed_base_field<F: FixedPoints<pallas::Affine>>(
+        base: F,
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
     ) -> Result<(), Error> {
-        // nullifier_k
-        let nullifier_k = OrchardFixedBases::NullifierK;
         test_single_base(
             chip.clone(),
-            layouter.namespace(|| "nullifier_k"),
-            FixedPoint::from_inner(chip, nullifier_k),
-            nullifier_k.generator(),
+            layouter.namespace(|| "fixed base"),
+            FixedPoint::from_inner(chip, base.clone()),
+            base.generator(),
         )
     }
 
     #[allow(clippy::op_ref)]
-    fn test_single_base(
-        chip: EccChip<OrchardFixedBases>,
+    fn test_single_base<F: FixedPoints<pallas::Affine>>(
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
-        base: FixedPoint<pallas::Affine, EccChip<OrchardFixedBases>>,
+        base: FixedPoint<pallas::Affine, EccChip<F>>,
         base_val: pallas::Affine,
     ) -> Result<(), Error> {
         let column = chip.config().advices[0];
 
-        fn constrain_equal(
-            chip: EccChip<OrchardFixedBases>,
+        fn constrain_equal<F: FixedPoints<pallas::Affine>>(
+            chip: EccChip<F>,
             mut layouter: impl Layouter<pallas::Base>,
             base_val: pallas::Affine,
             scalar_val: pallas::Base,
-            result: Point<pallas::Affine, EccChip<OrchardFixedBases>>,
+            result: Point<pallas::Affine, EccChip<F>>,
         ) -> Result<(), Error> {
             // Move scalar from base field into scalar field (which always fits for Pallas).
             let scalar = pallas::Scalar::from_bytes(&scalar_val.to_bytes()).unwrap();

halo2-gadgets/halo2_ecc/src/chip/mul_fixed/full_width.rs
@@ -1,5 +1,5 @@
 use super::super::{
-    EccConfig, EccPoint, EccScalarFixed, FixedPoints, FIXED_BASE_WINDOW_SIZE, H, L_ORCHARD_SCALAR,
+    EccConfig, EccPoint, EccScalarFixed, FixedPoints, FIXED_BASE_WINDOW_SIZE, H, L_PALLAS_SCALAR,
     NUM_WINDOWS,
 };
 
@@ -54,7 +54,7 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
         offset: usize,
         scalar: Option<pallas::Scalar>,
     ) -> Result<EccScalarFixed, Error> {
-        let windows = self.decompose_scalar_fixed::<L_ORCHARD_SCALAR>(scalar, offset, region)?;
+        let windows = self.decompose_scalar_fixed::<L_PALLAS_SCALAR>(scalar, offset, region)?;
 
         Ok(EccScalarFixed {
             value: scalar,
@@ -159,7 +159,7 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
     use group::Curve;
     use halo2::{circuit::Layouter, plonk::Error};
@@ -169,64 +169,35 @@ pub mod tests {
         chip::EccChip,
         gadget::{FixedPoint, FixedPoints, Point, H},
     };
-    use orchard::constants::OrchardFixedBases;
 
-    pub fn test_mul_fixed(
-        chip: EccChip<OrchardFixedBases>,
+    pub fn test_mul_fixed<F: FixedPoints<pallas::Affine>>(
+        base: F,
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
     ) -> Result<(), Error> {
-        // commit_ivk_r
-        let commit_ivk_r = OrchardFixedBases::CommitIvkR;
         test_single_base(
             chip.clone(),
-            layouter.namespace(|| "commit_ivk_r"),
-            FixedPoint::from_inner(chip.clone(), commit_ivk_r),
-            commit_ivk_r.generator(),
-        )?;
-
-        // note_commit_r
-        let note_commit_r = OrchardFixedBases::NoteCommitR;
-        test_single_base(
-            chip.clone(),
-            layouter.namespace(|| "note_commit_r"),
-            FixedPoint::from_inner(chip.clone(), note_commit_r),
-            note_commit_r.generator(),
-        )?;
-
-        // value_commit_r
-        let value_commit_r = OrchardFixedBases::ValueCommitR;
-        test_single_base(
-            chip.clone(),
-            layouter.namespace(|| "value_commit_r"),
-            FixedPoint::from_inner(chip.clone(), value_commit_r),
-            value_commit_r.generator(),
-        )?;
-
-        // spend_auth_g
-        let spend_auth_g = OrchardFixedBases::SpendAuthG;
-        test_single_base(
-            chip.clone(),
-            layouter.namespace(|| "spend_auth_g"),
-            FixedPoint::from_inner(chip, spend_auth_g),
-            spend_auth_g.generator(),
+            layouter.namespace(|| "fixed base"),
+            FixedPoint::from_inner(chip, base.clone()),
+            base.generator(),
         )?;
 
         Ok(())
     }
 
     #[allow(clippy::op_ref)]
-    fn test_single_base(
-        chip: EccChip<OrchardFixedBases>,
+    fn test_single_base<F: FixedPoints<pallas::Affine>>(
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
-        base: FixedPoint<pallas::Affine, EccChip<OrchardFixedBases>>,
+        base: FixedPoint<pallas::Affine, EccChip<F>>,
         base_val: pallas::Affine,
     ) -> Result<(), Error> {
-        fn constrain_equal(
-            chip: EccChip<OrchardFixedBases>,
+        fn constrain_equal<F: FixedPoints<pallas::Affine>>(
+            chip: EccChip<F>,
             mut layouter: impl Layouter<pallas::Base>,
             base_val: pallas::Affine,
             scalar_val: pallas::Scalar,
-            result: Point<pallas::Affine, EccChip<OrchardFixedBases>>,
+            result: Point<pallas::Affine, EccChip<F>>,
         ) -> Result<(), Error> {
             let expected = Point::new(
                 chip,

zcash/orchard

halo2-gadgets/halo2_ecc/src/chip/add.rs
@@ -385,9 +385,9 @@ impl Config {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
-    use group::{prime::PrimeCurveAffine, Curve};
+    use group::{prime::PrimeCurveAffine, Curve, Group};
     use halo2::{circuit::Layouter, plonk::Error};
     use pasta_curves::{arithmetic::CurveExt, pallas};
 
@@ -399,21 +399,34 @@ pub mod tests {
     >(
         chip: EccChip,
         mut layouter: impl Layouter<pallas::Base>,
-        p_val: pallas::Affine,
-        p: &NonIdentityPoint<pallas::Affine, EccChip>,
-        q_val: pallas::Affine,
-        q: &NonIdentityPoint<pallas::Affine, EccChip>,
-        p_neg: &NonIdentityPoint<pallas::Affine, EccChip>,
     ) -> Result<(), Error> {
+        // Generate a random point P
+        let p_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // P
+        let p = NonIdentityPoint::new(chip.clone(), layouter.namespace(|| "P"), Some(p_val))?;
+        let p_neg = -p_val;
+        let p_neg = NonIdentityPoint::new(chip.clone(), layouter.namespace(|| "-P"), Some(p_neg))?;
+
+        // Generate a random point Q
+        let q_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // Q
+        let q = NonIdentityPoint::new(chip.clone(), layouter.namespace(|| "Q"), Some(q_val))?;
+
         // Make sure P and Q are not the same point.
         assert_ne!(p_val, q_val);
 
-        // Check complete addition P + (-P)
+        // Generate a (0,0) point to be used in other tests.
         let zero = {
-            let result = p.add(layouter.namespace(|| "P + (-P)"), p_neg)?;
-            assert!(result.inner().is_identity().unwrap());
-            result
+            Point::new(
+                chip.clone(),
+                layouter.namespace(|| "identity"),
+                Some(pallas::Affine::identity()),
+            )?
         };
+
+        // Check complete addition P + (-P)
+        {
+            let result = p.add(layouter.namespace(|| "P + (-P)"), &p_neg)?;
+            result.constrain_equal(layouter.namespace(|| "P + (-P) = 𝒪"), &zero)?;
+        }
 
         // Check complete addition 𝒪 + 𝒪
         {
@@ -423,7 +436,7 @@ pub mod tests {
 
         // Check P + Q
         {
-            let result = p.add(layouter.namespace(|| "P + Q"), q)?;
+            let result = p.add(layouter.namespace(|| "P + Q"), &q)?;
             let witnessed_result = NonIdentityPoint::new(
                 chip.clone(),
                 layouter.namespace(|| "witnessed P + Q"),
@@ -434,7 +447,7 @@ pub mod tests {
 
         // P + P
         {
-            let result = p.add(layouter.namespace(|| "P + P"), p)?;
+            let result = p.add(layouter.namespace(|| "P + P"), &p)?;
             let witnessed_result = NonIdentityPoint::new(
                 chip.clone(),
                 layouter.namespace(|| "witnessed P + P"),
@@ -446,13 +459,13 @@ pub mod tests {
         // P + 𝒪
         {
             let result = p.add(layouter.namespace(|| "P + 𝒪"), &zero)?;
-            result.constrain_equal(layouter.namespace(|| "P + 𝒪 = P"), p)?;
+            result.constrain_equal(layouter.namespace(|| "P + 𝒪 = P"), &p)?;
         }
 
         // 𝒪 + P
         {
-            let result = zero.add(layouter.namespace(|| "𝒪 + P"), p)?;
-            result.constrain_equal(layouter.namespace(|| "𝒪 + P = P"), p)?;
+            let result = zero.add(layouter.namespace(|| "𝒪 + P"), &p)?;
+            result.constrain_equal(layouter.namespace(|| "𝒪 + P = P"), &p)?;
         }
 
         // (x, y) + (ζx, y) should behave like normal P + Q.

halo2-gadgets/halo2_ecc/src/chip/add_incomplete.rs
@@ -143,9 +143,9 @@ impl Config {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
-    use group::Curve;
+    use group::{prime::PrimeCurveAffine, Curve, Group};
     use halo2::{circuit::Layouter, plonk::Error};
     use pasta_curves::pallas;
 
@@ -157,15 +157,32 @@ pub mod tests {
     >(
         chip: EccChip,
         mut layouter: impl Layouter<pallas::Base>,
-        p_val: pallas::Affine,
-        p: &NonIdentityPoint<pallas::Affine, EccChip>,
-        q_val: pallas::Affine,
-        q: &NonIdentityPoint<pallas::Affine, EccChip>,
-        p_neg: &NonIdentityPoint<pallas::Affine, EccChip>,
     ) -> Result<(), Error> {
+        // Generate a random point P
+        let p_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // P
+        let p = NonIdentityPoint::new(chip.clone(), layouter.namespace(|| "P"), Some(p_val))?;
+        let p_neg = -p_val;
+        let p_neg = NonIdentityPoint::new(chip.clone(), layouter.namespace(|| "-P"), Some(p_neg))?;
+
+        // Generate a random point Q
+        let q_val = pallas::Point::random(rand::rngs::OsRng).to_affine(); // Q
+        let q = NonIdentityPoint::new(chip.clone(), layouter.namespace(|| "Q"), Some(q_val))?;
+
+        // Make sure P and Q are not the same point.
+        assert_ne!(p_val, q_val);
+
+        // Generate a (0,0) point to be used in other tests.
+        let zero = {
+            Point::new(
+                chip.clone(),
+                layouter.namespace(|| "identity"),
+                Some(pallas::Affine::identity()),
+            )?
+        };
+
         // P + Q
         {
-            let result = p.add_incomplete(layouter.namespace(|| "P + Q"), q)?;
+            let result = p.add_incomplete(layouter.namespace(|| "P + Q"), &q)?;
             let witnessed_result = NonIdentityPoint::new(
                 chip,
                 layouter.namespace(|| "witnessed P + Q"),
@@ -175,11 +192,11 @@ pub mod tests {
         }
 
         // P + P should return an error
-        p.add_incomplete(layouter.namespace(|| "P + P"), p)
+        p.add_incomplete(layouter.namespace(|| "P + P"), &p)
             .expect_err("P + P should return an error");
 
         // P + (-P) should return an error
-        p.add_incomplete(layouter.namespace(|| "P + (-P)"), p_neg)
+        p.add_incomplete(layouter.namespace(|| "P + (-P)"), &p_neg)
             .expect_err("P + (-P) should return an error");
 
         Ok(())

halo2-gadgets/halo2_ecc/src/chip/mul_fixed/base_field_elem.rs
@@ -1,6 +1,6 @@
 use super::super::{
     EccBaseFieldElemFixed, EccConfig, EccPoint, FixedPoints, FIXED_BASE_WINDOW_SIZE, LOOKUP_K,
-    L_ORCHARD_BASE, NUM_WINDOWS, T_P,
+    L_PALLAS_BASE, NUM_WINDOWS, T_P,
 };
 use super::H_BASE;
 use utilities::{
@@ -170,7 +170,7 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
                         offset,
                         scalar,
                         true,
-                        L_ORCHARD_BASE,
+                        L_PALLAS_BASE,
                         NUM_WINDOWS,
                     )?;
                     EccBaseFieldElemFixed {
@@ -375,9 +375,9 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
-    use group::Curve;
+    use group::{Curve, Group};
     use halo2::{
         circuit::{Chip, Layouter},
         plonk::Error,
@@ -385,41 +385,47 @@ pub mod tests {
     use pasta_curves::{arithmetic::FieldExt, pallas};
 
     use crate::{
-        chip::EccChip,
+        chip::{EccChip, NUM_WINDOWS},
         gadget::{FixedPoint, FixedPoints, NonIdentityPoint, Point, H},
     };
-    use orchard::constants::OrchardFixedBases;
     use utilities::UtilitiesInstructions;
 
-    pub fn test_mul_fixed_base_field(
-        chip: EccChip<OrchardFixedBases>,
+    use lazy_static::lazy_static;
+
+    lazy_static! {
+        static ref BASE: pallas::Affine = pallas::Point::generator().to_affine();
+        static ref ZS_AND_US: Vec<(u64, [[u8; 32]; H])> =
+            crate::chip::find_zs_and_us(*BASE, NUM_WINDOWS).unwrap();
+    }
+
+    pub fn test_mul_fixed_base_field<F: FixedPoints<pallas::Affine>>(
+        base: F,
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
     ) -> Result<(), Error> {
-        // nullifier_k
-        let nullifier_k = OrchardFixedBases::NullifierK;
         test_single_base(
             chip.clone(),
-            layouter.namespace(|| "nullifier_k"),
-            FixedPoint::from_inner(chip, nullifier_k),
-            nullifier_k.generator(),
+            layouter.namespace(|| "fixed base"),
+            FixedPoint::from_inner(chip, base.clone()),
+            base.generator(),
         )
     }
 
     #[allow(clippy::op_ref)]
-    fn test_single_base(
-        chip: EccChip<OrchardFixedBases>,
+    fn test_single_base<F: FixedPoints<pallas::Affine>>(
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
-        base: FixedPoint<pallas::Affine, EccChip<OrchardFixedBases>>,
+        base: FixedPoint<pallas::Affine, EccChip<F>>,
         base_val: pallas::Affine,
     ) -> Result<(), Error> {
         let column = chip.config().advices[0];
 
-        fn constrain_equal_non_id(
-            chip: EccChip<OrchardFixedBases>,
+        fn constrain_equal_non_id<F: FixedPoints<pallas::Affine>>(
+            chip: EccChip<F>,
             mut layouter: impl Layouter<pallas::Base>,
             base_val: pallas::Affine,
             scalar_val: pallas::Base,
-            result: Point<pallas::Affine, EccChip<OrchardFixedBases>>,
+            result: Point<pallas::Affine, EccChip<F>>,
         ) -> Result<(), Error> {
             // Move scalar from base field into scalar field (which always fits for Pallas).
             let scalar = pallas::Scalar::from_bytes(&scalar_val.to_bytes()).unwrap();

halo2-gadgets/halo2_ecc/src/chip/mul_fixed/full_width.rs
@@ -1,5 +1,5 @@
 use super::super::{
-    EccConfig, EccPoint, EccScalarFixed, FixedPoints, FIXED_BASE_WINDOW_SIZE, H, L_ORCHARD_SCALAR,
+    EccConfig, EccPoint, EccScalarFixed, FixedPoints, FIXED_BASE_WINDOW_SIZE, H, L_PALLAS_SCALAR,
     NUM_WINDOWS,
 };
 
@@ -54,7 +54,7 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
         offset: usize,
         scalar: Option<pallas::Scalar>,
     ) -> Result<EccScalarFixed, Error> {
-        let windows = self.decompose_scalar_fixed::<L_ORCHARD_SCALAR>(scalar, offset, region)?;
+        let windows = self.decompose_scalar_fixed::<L_PALLAS_SCALAR>(scalar, offset, region)?;
 
         Ok(EccScalarFixed {
             value: scalar,
@@ -162,7 +162,7 @@ impl<Fixed: FixedPoints<pallas::Affine>> Config<Fixed> {
     }
 }
 
-#[cfg(test)]
+#[cfg(feature = "testing")]
 pub mod tests {
     use group::Curve;
     use halo2::{circuit::Layouter, plonk::Error};
@@ -172,64 +172,35 @@ pub mod tests {
         chip::EccChip,
         gadget::{FixedPoint, FixedPoints, NonIdentityPoint, Point, H},
     };
-    use orchard::constants::OrchardFixedBases;
 
-    pub fn test_mul_fixed(
-        chip: EccChip<OrchardFixedBases>,
+    pub fn test_mul_fixed<F: FixedPoints<pallas::Affine>>(
+        base: F,
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
     ) -> Result<(), Error> {
-        // commit_ivk_r
-        let commit_ivk_r = OrchardFixedBases::CommitIvkR;
         test_single_base(
             chip.clone(),
-            layouter.namespace(|| "commit_ivk_r"),
-            FixedPoint::from_inner(chip.clone(), commit_ivk_r),
-            commit_ivk_r.generator(),
-        )?;
-
-        // note_commit_r
-        let note_commit_r = OrchardFixedBases::NoteCommitR;
-        test_single_base(
-            chip.clone(),
-            layouter.namespace(|| "note_commit_r"),
-            FixedPoint::from_inner(chip.clone(), note_commit_r),
-            note_commit_r.generator(),
-        )?;
-
-        // value_commit_r
-        let value_commit_r = OrchardFixedBases::ValueCommitR;
-        test_single_base(
-            chip.clone(),
-            layouter.namespace(|| "value_commit_r"),
-            FixedPoint::from_inner(chip.clone(), value_commit_r),
-            value_commit_r.generator(),
-        )?;
-
-        // spend_auth_g
-        let spend_auth_g = OrchardFixedBases::SpendAuthG;
-        test_single_base(
-            chip.clone(),
-            layouter.namespace(|| "spend_auth_g"),
-            FixedPoint::from_inner(chip, spend_auth_g),
-            spend_auth_g.generator(),
+            layouter.namespace(|| "fixed base"),
+            FixedPoint::from_inner(chip, base.clone()),
+            base.generator(),
         )?;
 
         Ok(())
     }
 
     #[allow(clippy::op_ref)]
-    fn test_single_base(
-        chip: EccChip<OrchardFixedBases>,
+    fn test_single_base<F: FixedPoints<pallas::Affine>>(
+        chip: EccChip<F>,
         mut layouter: impl Layouter<pallas::Base>,
-        base: FixedPoint<pallas::Affine, EccChip<OrchardFixedBases>>,
+        base: FixedPoint<pallas::Affine, EccChip<F>>,
         base_val: pallas::Affine,
     ) -> Result<(), Error> {
-        fn constrain_equal_non_id(
-            chip: EccChip<OrchardFixedBases>,
+        fn constrain_equal_non_id<F: FixedPoints<pallas::Affine>>(
+            chip: EccChip<F>,
             mut layouter: impl Layouter<pallas::Base>,
             base_val: pallas::Affine,
             scalar_val: pallas::Scalar,
-            result: Point<pallas::Affine, EccChip<OrchardFixedBases>>,
+            result: Point<pallas::Affine, EccChip<F>>,
         ) -> Result<(), Error> {
             let expected = NonIdentityPoint::new(
                 chip,