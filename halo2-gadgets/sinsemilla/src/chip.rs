therealyingtong/halo2-gadgets

Remove from_coordinates_unchecked() API, introduce copy_point()
Now that the Sinsemilla and ECC instructions are in different
crates, ECC can no longer limit from_coordinates_unchecked()
to just the crate.

Instead of exposing this API publically, we introduce a copy_point()
instruction that allows the output of Sinsemilla hash_to_point to be
copied and checked by the ECC chip.

halo2-gadgets/sinsemilla/src/chip.rs
@@ -1,11 +1,13 @@
 use super::{
-    gadget::{CommitDomains, HashDomains, SinsemillaInstructions},
+    gadget::{self, CommitDomains, HashDomains, SinsemillaInstructions},
     message::{Message, MessagePiece},
     primitive as sinsemilla,
 };
-use ecc::{chip::EccPoint, gadget::FixedPoints};
+use ecc::gadget::FixedPoints;
 use std::marker::PhantomData;
-use utilities::{lookup_range_check::LookupRangeCheckConfig, CellValue, Var};
+use utilities::{
+    lookup_range_check::LookupRangeCheckConfig, CellValue, UtilitiesInstructions, Var,
+};
 
 use halo2::{
     circuit::{Chip, Layouter},
@@ -21,6 +23,22 @@ mod generator_table;
 use generator_table::GeneratorTableConfig;
 
 mod hash_to_point;
+
+/// A point output by hash_to_point
+#[derive(Clone, Debug)]
+pub struct Point {
+    x: CellValue<pallas::Base>,
+    y: CellValue<pallas::Base>,
+}
+
+impl gadget::Point<pallas::Affine, CellValue<pallas::Base>> for Point {
+    fn x(&self) -> CellValue<pallas::Base> {
+        self.x
+    }
+    fn y(&self) -> CellValue<pallas::Base> {
+        self.y
+    }
+}
 
 /// Configuration for the Sinsemilla hash chip
 #[derive(Eq, PartialEq, Clone, Debug)]
@@ -92,6 +110,16 @@ where
     Fixed: FixedPoints<pallas::Affine>,
 {
     config: SinsemillaConfig<Hash, Commit, Fixed>,
+}
+
+impl<Hash, Commit, Fixed> UtilitiesInstructions<pallas::Base>
+    for SinsemillaChip<Hash, Commit, Fixed>
+where
+    Hash: HashDomains<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, Fixed, Hash>,
+    Fixed: FixedPoints<pallas::Affine>,
+{
+    type Var = CellValue<pallas::Base>;
 }
 
 impl<Hash, Commit, Fixed> Chip<pallas::Base> for SinsemillaChip<Hash, Commit, Fixed>
@@ -268,15 +296,13 @@ where
     F: FixedPoints<pallas::Affine>,
     Commit: CommitDomains<pallas::Affine, F, Hash>,
 {
-    type CellValue = CellValue<pallas::Base>;
-
     type Message = Message<pallas::Base, { sinsemilla::K }, { sinsemilla::C }>;
     type MessagePiece = MessagePiece<pallas::Base, { sinsemilla::K }>;
 
-    type RunningSum = Vec<Self::CellValue>;
+    type RunningSum = Vec<Self::Var>;
 
-    type X = CellValue<pallas::Base>;
-    type Point = EccPoint;
+    type X = Self::Var;
+    type Point = Point;
     type FixedPoints = F;
 
     type HashDomains = Hash;
@@ -319,6 +345,6 @@ where
     }
 
     fn extract(point: &Self::Point) -> Self::X {
-        point.x()
+        point.x
     }
 }

halo2-gadgets/sinsemilla/src/gadget.rs
@@ -4,16 +4,15 @@ use ff::PrimeField;
 use halo2::{circuit::Layouter, plonk::Error};
 use pasta_curves::arithmetic::{CurveAffine, FieldExt};
 use std::{convert::TryInto, fmt::Debug};
-use utilities::Var;
+use utilities::{UtilitiesInstructions, Var};
 
 /// The set of circuit instructions required to use the [`Sinsemilla`](https://zcash.github.io/halo2/design/gadgets/sinsemilla.html) gadget.
 /// This trait is bounded on two constant parameters: `K`, the number of bits
 /// in each word accepted by the Sinsemilla hash, and `MAX_WORDS`, the maximum
 /// number of words that a single hash instance can process.
-pub trait SinsemillaInstructions<C: CurveAffine, const K: usize, const MAX_WORDS: usize> {
-    /// A variable in the circuit.
-    type CellValue: Var<C::Base>;
-
+pub trait SinsemillaInstructions<C: CurveAffine, const K: usize, const MAX_WORDS: usize>:
+    UtilitiesInstructions<C::Base>
+{
     /// A message composed of [`Self::MessagePiece`]s.
     type Message: From<Vec<Self::MessagePiece>>;
 
@@ -39,7 +38,7 @@ pub trait SinsemillaInstructions<C: CurveAffine, const K: usize, const MAX_WORDS
     /// The x-coordinate of a point output of [`Self::hash_to_point`].
     type X;
     /// A point output of [`Self::hash_to_point`].
-    type Point: Clone + Debug;
+    type Point: Point<C, Self::Var>;
     /// A type enumerating the fixed points used in `CommitDomains`.
     type FixedPoints: FixedPoints<C>;
 
@@ -232,8 +231,8 @@ pub struct HashDomain<
     SinsemillaChip: SinsemillaInstructions<C, K, MAX_WORDS> + Clone + Debug + Eq,
     EccChip: EccInstructions<
             C,
-            Point = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::Point,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,
@@ -249,8 +248,8 @@ where
     SinsemillaChip: SinsemillaInstructions<C, K, MAX_WORDS> + Clone + Debug + Eq,
     EccChip: EccInstructions<
             C,
-            Point = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::Point,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,
@@ -275,13 +274,18 @@ where
     /// [concretesinsemillahash]: https://zips.z.cash/protocol/protocol.pdf#concretesinsemillahash
     pub fn hash_to_point(
         &self,
-        layouter: impl Layouter<C::Base>,
+        mut layouter: impl Layouter<C::Base>,
         message: Message<C, SinsemillaChip, K, MAX_WORDS>,
     ) -> Result<(Ecc::Point<C, EccChip>, Vec<SinsemillaChip::RunningSum>), Error> {
         assert_eq!(self.sinsemilla_chip, message.chip);
-        self.sinsemilla_chip
-            .hash_to_point(layouter, self.Q, message.inner)
-            .map(|(point, zs)| (Ecc::Point::from_inner(self.ecc_chip.clone(), point), zs))
+        let (point, zs) = self.sinsemilla_chip.hash_to_point(
+            layouter.namespace(|| "hash_to_point"),
+            self.Q,
+            message.inner,
+        )?;
+        let point = Ecc::Point::copy(self.ecc_chip.clone(), layouter, point.x(), point.y())?;
+
+        Ok((point, zs))
     }
 
     /// $\mathsf{SinsemillaHash}$ from [§ 5.4.1.9][concretesinsemillahash].
@@ -297,6 +301,12 @@ where
         let (p, zs) = self.hash_to_point(layouter, message)?;
         Ok((p.extract_p(), zs))
     }
+}
+
+/// Trait defining behaviour of a point output by Sinsemilla hash-to-point.
+pub trait Point<C: CurveAffine, V: Var<C::Base>> {
+    fn x(&self) -> V;
+    fn y(&self) -> V;
 }
 
 /// Trait allowing circuit's Sinsemilla CommitDomains to be enumerated.
@@ -328,8 +338,8 @@ pub struct CommitDomain<
     SinsemillaChip: SinsemillaInstructions<C, K, MAX_WORDS> + Clone + Debug + Eq,
     EccChip: EccInstructions<
             C,
-            Point = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::Point,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,
@@ -344,8 +354,8 @@ where
     SinsemillaChip: SinsemillaInstructions<C, K, MAX_WORDS> + Clone + Debug + Eq,
     EccChip: EccInstructions<
             C,
-            Point = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::Point,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,

halo2-gadgets/sinsemilla/src/merkle/chip.rs
@@ -425,12 +425,6 @@ where
     F: FixedPoints<pallas::Affine>,
     Commit: CommitDomains<pallas::Affine, F, Hash>,
 {
-    type CellValue = <SinsemillaChip<Hash, Commit, F> as SinsemillaInstructions<
-        pallas::Affine,
-        { sinsemilla::K },
-        { sinsemilla::C },
-    >>::CellValue;
-
     type Message = <SinsemillaChip<Hash, Commit, F> as SinsemillaInstructions<
         pallas::Affine,
         { sinsemilla::K },
@@ -492,7 +486,7 @@ where
         layouter: impl Layouter<pallas::Base>,
         Q: pallas::Affine,
         message: Self::Message,
-    ) -> Result<(Self::Point, Vec<Vec<Self::CellValue>>), Error> {
+    ) -> Result<(Self::Point, Vec<Vec<Self::Var>>), Error> {
         let config = self.config().sinsemilla_config.clone();
         let chip = SinsemillaChip::<Hash, Commit, F>::construct(config);
         chip.hash_to_point(layouter, Q, message)

zcash/orchard

halo2-gadgets/sinsemilla/src/chip.rs
@@ -1,11 +1,13 @@
 use super::{
-    gadget::{CommitDomains, HashDomains, SinsemillaInstructions},
+    gadget::{self, CommitDomains, HashDomains, SinsemillaInstructions},
     message::{Message, MessagePiece},
     primitive as sinsemilla,
 };
-use ecc::{chip::EccPoint, gadget::FixedPoints};
+use ecc::gadget::FixedPoints;
 use std::marker::PhantomData;
-use utilities::{lookup_range_check::LookupRangeCheckConfig, CellValue, Var};
+use utilities::{
+    lookup_range_check::LookupRangeCheckConfig, CellValue, UtilitiesInstructions, Var,
+};
 
 use halo2::{
     circuit::{Chip, Layouter},
@@ -21,6 +23,22 @@ mod generator_table;
 use generator_table::GeneratorTableConfig;
 
 mod hash_to_point;
+
+/// A point output by hash_to_point
+#[derive(Clone, Debug)]
+struct Point {
+    x: CellValue<pallas::Base>,
+    y: CellValue<pallas::Base>,
+}
+
+impl gadget::Point<pallas::Affine, CellValue<pallas::Base>> for Point {
+    fn x(&self) -> CellValue<pallas::Base> {
+        self.x
+    }
+    fn y(&self) -> CellValue<pallas::Base> {
+        self.y
+    }
+}
 
 /// Configuration for the Sinsemilla hash chip
 #[derive(Eq, PartialEq, Clone, Debug)]
@@ -92,6 +110,16 @@ where
     Fixed: FixedPoints<pallas::Affine>,
 {
     config: SinsemillaConfig<Hash, Commit, Fixed>,
+}
+
+impl<Hash, Commit, Fixed> UtilitiesInstructions<pallas::Base>
+    for SinsemillaChip<Hash, Commit, Fixed>
+where
+    Hash: HashDomains<pallas::Affine>,
+    Commit: CommitDomains<pallas::Affine, Fixed, Hash>,
+    Fixed: FixedPoints<pallas::Affine>,
+{
+    type Var = CellValue<pallas::Base>;
 }
 
 impl<Hash, Commit, Fixed> Chip<pallas::Base> for SinsemillaChip<Hash, Commit, Fixed>
@@ -268,15 +296,13 @@ where
     F: FixedPoints<pallas::Affine>,
     Commit: CommitDomains<pallas::Affine, F, Hash>,
 {
-    type CellValue = CellValue<pallas::Base>;
-
     type Message = Message<pallas::Base, { sinsemilla::K }, { sinsemilla::C }>;
     type MessagePiece = MessagePiece<pallas::Base, { sinsemilla::K }>;
 
-    type RunningSum = Vec<Self::CellValue>;
+    type RunningSum = Vec<Self::Var>;
 
-    type X = CellValue<pallas::Base>;
-    type NonIdentityPoint = NonIdentityEccPoint;
+    type X = Self::Var;
+    type NonIdentityPoint = Point;
     type FixedPoints = F;
 
     type HashDomains = Hash;

halo2-gadgets/sinsemilla/src/gadget.rs
@@ -4,16 +4,15 @@ use ff::PrimeField;
 use halo2::{circuit::Layouter, plonk::Error};
 use pasta_curves::arithmetic::{CurveAffine, FieldExt};
 use std::{convert::TryInto, fmt::Debug};
-use utilities::Var;
+use utilities::{UtilitiesInstructions, Var};
 
 /// The set of circuit instructions required to use the [`Sinsemilla`](https://zcash.github.io/halo2/design/gadgets/sinsemilla.html) gadget.
 /// This trait is bounded on two constant parameters: `K`, the number of bits
 /// in each word accepted by the Sinsemilla hash, and `MAX_WORDS`, the maximum
 /// number of words that a single hash instance can process.
-pub trait SinsemillaInstructions<C: CurveAffine, const K: usize, const MAX_WORDS: usize> {
-    /// A variable in the circuit.
-    type CellValue: Var<C::Base>;
-
+pub trait SinsemillaInstructions<C: CurveAffine, const K: usize, const MAX_WORDS: usize>:
+    UtilitiesInstructions<C::Base>
+{
     /// A message composed of [`Self::MessagePiece`]s.
     type Message: From<Vec<Self::MessagePiece>>;
 
@@ -39,7 +38,7 @@ pub trait SinsemillaInstructions<C: CurveAffine, const K: usize, const MAX_WORDS
     /// The x-coordinate of a point output of [`Self::hash_to_point`].
     type X;
     /// A point output of [`Self::hash_to_point`].
-    type NonIdentityPoint: Clone + Debug;
+    type NonIdentityPoint: Clone + Debug + Point<C, Self::Var>;
     /// A type enumerating the fixed points used in `CommitDomains`.
     type FixedPoints: FixedPoints<C>;
 
@@ -234,6 +233,7 @@ pub struct HashDomain<
             C,
             NonIdentityPoint = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::NonIdentityPoint,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,
@@ -251,6 +251,7 @@ where
             C,
             NonIdentityPoint = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::NonIdentityPoint,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,
@@ -275,13 +276,18 @@ where
     /// [concretesinsemillahash]: https://zips.z.cash/protocol/protocol.pdf#concretesinsemillahash
     pub fn hash_to_point(
         &self,
-        layouter: impl Layouter<C::Base>,
+        mut layouter: impl Layouter<C::Base>,
         message: Message<C, SinsemillaChip, K, MAX_WORDS>,
     ) -> Result<(Ecc::NonIdentityPoint<C, EccChip>, Vec<SinsemillaChip::RunningSum>), Error> {
         assert_eq!(self.sinsemilla_chip, message.chip);
-        self.sinsemilla_chip
-            .hash_to_point(layouter, self.Q, message.inner)
-            .map(|(point, zs)| (Ecc::NonIdentityPoint::from_inner(self.ecc_chip.clone(), point), zs))
+        let (point, zs) = self.sinsemilla_chip.hash_to_point(
+            layouter.namespace(|| "hash_to_point"),
+            self.Q,
+            message.inner,
+        )?;
+        let point = Ecc::NonIdentityPoint::copy(self.ecc_chip.clone(), layouter, point.x(), point.y())?;
+
+        Ok((point, zs))
     }
 
     /// $\mathsf{SinsemillaHash}$ from [§ 5.4.1.9][concretesinsemillahash].
@@ -297,6 +303,12 @@ where
         let (p, zs) = self.hash_to_point(layouter, message)?;
         Ok((p.extract_p(), zs))
     }
+}
+
+/// Trait defining behaviour of a point output by Sinsemilla hash-to-point.
+pub trait Point<C: CurveAffine, V: Var<C::Base>> {
+    fn x(&self) -> V;
+    fn y(&self) -> V;
 }
 
 /// Trait allowing circuit's Sinsemilla CommitDomains to be enumerated.
@@ -330,6 +342,7 @@ pub struct CommitDomain<
             C,
             NonIdentityPoint = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::NonIdentityPoint,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,
@@ -346,6 +359,7 @@ where
             C,
             NonIdentityPoint = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::NonIdentityPoint,
             FixedPoints = <SinsemillaChip as SinsemillaInstructions<C, K, MAX_WORDS>>::FixedPoints,
+            Var = SinsemillaChip::Var,
         > + Clone
         + Debug
         + Eq,

halo2-gadgets/sinsemilla/src/merkle/chip.rs
@@ -425,12 +425,6 @@ where
     F: FixedPoints<pallas::Affine>,
     Commit: CommitDomains<pallas::Affine, F, Hash>,
 {
-    type CellValue = <SinsemillaChip<Hash, Commit, F> as SinsemillaInstructions<
-        pallas::Affine,
-        { sinsemilla::K },
-        { sinsemilla::C },
-    >>::CellValue;
-
     type Message = <SinsemillaChip<Hash, Commit, F> as SinsemillaInstructions<
         pallas::Affine,
         { sinsemilla::K },
@@ -492,7 +486,7 @@ where
         layouter: impl Layouter<pallas::Base>,
         Q: pallas::Affine,
         message: Self::Message,
-    ) -> Result<(Self::NonIdentityPoint, Vec<Vec<Self::CellValue>>), Error> {
+    ) -> Result<(Self::NonIdentityPoint, Vec<Vec<Self::Var>>), Error> {
         let config = self.config().sinsemilla_config.clone();
         let chip = SinsemillaChip::<Hash, Commit, F>::construct(config);
         chip.hash_to_point(layouter, Q, message)