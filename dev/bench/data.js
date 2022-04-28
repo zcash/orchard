6978b3377b55dd6052e131f2d1c6f476d1afc69a
github-action-benchmark <github@users.noreply.github.com>
2022-04-28 16:55:38 +0000

    add Orchard Benchmarks (cargo) benchmark result for 5e98c7d54cfb3ad1545d5ee8e89a80def36f3c2f

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1651164938096,
+  "repoUrl": "https://github.com/zcash/orchard",
+  "entries": {
+    "Orchard Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "jack@electriccoin.co",
+            "name": "str4d",
+            "username": "str4d"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e98c7d54cfb3ad1545d5ee8e89a80def36f3c2f",
+          "message": "Merge pull request #277 from zcash/book-delete-gadgets\n\n[book] Remove gadgets documentation.",
+          "timestamp": "2022-04-28T17:41:04+01:00",
+          "tree_id": "7b8899c365c80d8b33636aa85d968dd750ea366d",
+          "url": "https://github.com/zcash/orchard/commit/5e98c7d54cfb3ad1545d5ee8e89a80def36f3c2f"
+        },
+        "date": 1651164936961,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "proving/bundle/1",
+            "value": 5073289901,
+            "range": "± 35303239",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/2",
+            "value": 5109944842,
+            "range": "± 56532092",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/3",
+            "value": 7275439020,
+            "range": "± 78784452",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/4",
+            "value": 9439553996,
+            "range": "± 38669238",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/1",
+            "value": 39741933,
+            "range": "± 2142457",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/2",
+            "value": 40419879,
+            "range": "± 1973356",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/3",
+            "value": 45022542,
+            "range": "± 1775617",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "verifying/bundle/4",
+            "value": 50655127,
+            "range": "± 12453879",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
