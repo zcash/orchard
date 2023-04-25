7d3b6dfe96686c04bc52c754864a675ea778895b
Constance Beguier <constance@qed-it.com>
2023-04-25 14:46:26 +0200

    Constant-time note commitment for ZEC and ZSA (#54)
    We would like to have a constant-time evaluation of the note commitment for both ZEC and ZSA.
    ZEC_note_commitment=Extract_P(SinsemillaHashToPoint(zec_personalization, common_bits) + [rcm]R)
    ZSA_note_commitment=Extract_P(SinsemillaHashToPoint(zsa_personalization, common_bits || asset) + [rcm]R)
    
    R is the same constant for ZEC and ZSA note commitments.

--- src/note/commitment.rs
+++ src/note/commitment.rs
@@ -4,7 +4,7 @@ use bitvec::{array::BitArray, order::Lsb0};
 use group::ff::{PrimeField, PrimeFieldBits};
 use halo2_gadgets::sinsemilla::primitives as sinsemilla;
 use pasta_curves::pallas;
-use subtle::{ConstantTimeEq, CtOption};
+use subtle::{ConditionallySelectable, ConstantTimeEq, CtOption};
 
 use crate::{
     constants::{
@@ -56,35 +56,42 @@ impl NoteCommitment {
         let rho_bits = rho.to_le_bits();
         let psi_bits = psi.to_le_bits();
 
-        let zec_note_bits = iter::empty()
+        let common_note_bits = iter::empty()
             .chain(g_d_bits.iter().by_vals())
             .chain(pk_d_bits.iter().by_vals())
             .chain(v_bits.iter().by_vals())
             .chain(rho_bits.iter().by_vals().take(L_ORCHARD_BASE))
-            .chain(psi_bits.iter().by_vals().take(L_ORCHARD_BASE));
+            .chain(psi_bits.iter().by_vals().take(L_ORCHARD_BASE))
+            .collect::<Vec<bool>>();
 
-        // TODO: make this constant-time.
-        if asset.is_native().into() {
-            // Commit to ZEC notes as per the Orchard protocol.
-            Self::commit(NOTE_COMMITMENT_PERSONALIZATION, zec_note_bits, rcm)
-        } else {
-            // Commit to non-ZEC notes as per the ZSA protocol.
-            // Append the note type to the Orchard note encoding.
-            let type_bits = BitArray::<_, Lsb0>::new(asset.to_bytes());
-            let zsa_note_bits = zec_note_bits.chain(type_bits.iter().by_vals());
+        let zec_note_bits = common_note_bits.clone().into_iter();
 
-            // Commit in a different domain than Orchard notes.
-            Self::commit(NOTE_ZSA_COMMITMENT_PERSONALIZATION, zsa_note_bits, rcm)
-        }
-    }
+        let type_bits = BitArray::<_, Lsb0>::new(asset.to_bytes());
+        let zsa_note_bits = common_note_bits
+            .into_iter()
+            .chain(type_bits.iter().by_vals());
 
-    fn commit(
-        personalization: &str,
-        bits: impl Iterator<Item = bool>,
-        rcm: NoteCommitTrapdoor,
-    ) -> CtOption<Self> {
-        let domain = sinsemilla::CommitDomain::new(personalization);
-        domain.commit(bits, &rcm.0).map(NoteCommitment)
+        let zec_domain = sinsemilla::CommitDomain::new(NOTE_COMMITMENT_PERSONALIZATION);
+        let zsa_domain = sinsemilla::CommitDomain::new_with_personalization(
+            NOTE_ZSA_COMMITMENT_PERSONALIZATION,
+            NOTE_COMMITMENT_PERSONALIZATION,
+        );
+
+        let zec_hash_point = zec_domain.M.hash_to_point(zec_note_bits);
+        let zsa_hash_point = zsa_domain.M.hash_to_point(zsa_note_bits);
+
+        // Select the desired hash point in constant-time
+        let hash_point = zsa_hash_point.and_then(|zsa_hash| {
+            zec_hash_point.map(|zec_hash| {
+                pallas::Point::conditional_select(&zsa_hash, &zec_hash, asset.is_native())
+            })
+        });
+
+        // To evaluate the commitment from the hash_point, we could use either zec_domain or
+        // zsa_domain because they have both the same `R` constant.
+        zec_domain
+            .commit_from_hash_point(hash_point, &rcm.0)
+            .map(NoteCommitment)
     }
 }
 
@@ -140,3 +147,40 @@ impl PartialEq for ExtractedNoteCommitment {
 }
 
 impl Eq for ExtractedNoteCommitment {}
+
+#[cfg(test)]
+mod tests {
+    use crate::constants::fixed_bases::{
+        NOTE_COMMITMENT_PERSONALIZATION, NOTE_ZSA_COMMITMENT_PERSONALIZATION,
+    };
+    use crate::note::commitment::NoteCommitTrapdoor;
+    use ff::Field;
+    use halo2_gadgets::sinsemilla::primitives as sinsemilla;
+    use pasta_curves::pallas;
+    use rand::{rngs::OsRng, Rng};
+
+    #[test]
+    fn test_commit_in_several_steps() {
+        let mut os_rng = OsRng::default();
+        let msg: Vec<bool> = (0..36).map(|_| os_rng.gen::<bool>()).collect();
+
+        let rcm = NoteCommitTrapdoor(pallas::Scalar::random(&mut os_rng));
+
+        let domain_zec = sinsemilla::CommitDomain::new(NOTE_COMMITMENT_PERSONALIZATION);
+        let domain_zsa = sinsemilla::CommitDomain::new_with_personalization(
+            NOTE_ZSA_COMMITMENT_PERSONALIZATION,
+            NOTE_COMMITMENT_PERSONALIZATION,
+        );
+
+        let expected_commit = domain_zsa.commit(msg.clone().into_iter(), &rcm.0);
+
+        // Evaluating the commitment in one step with `commit` or in two steps with `hash_to_point`
+        // and `commit_from_hash_point` must give the same commitment.
+        let hash_point = domain_zsa.M.hash_to_point(msg.into_iter());
+        let commit_r_zsa = domain_zsa.commit_from_hash_point(hash_point, &rcm.0);
+        assert_eq!(expected_commit.unwrap(), commit_r_zsa.unwrap());
+
+        // ZEC and ZSA note commitments must use the same R constant
+        assert_eq!(domain_zec.R(), domain_zsa.R());
+    }
+}
